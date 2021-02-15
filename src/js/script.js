2ffe4d738e6c0ad7fb74c8afc7b6fe94aa678e70
Karol Wojtaś <karol.wojtas87@gmail.com>
2021-02-15 21:15:07 +0100

    New repository

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+{
+  'use strict';
+
+  const templates = Handlebars.compile(document.querySelector('#template-book').innerHTML);
+
+  const listOfBooks = document.querySelector('books-list');
+
+  function render() {
+    for (const parameter of dataSource.books) {
+      parameter
+    }
+  }
+
+
+
+
+}
